181956c6f4b05527ea54c76c2949fdb85234d3f1
Ricard Villalba Burrull <ricardvillalbaburrull@MacBook-MacBook-Pro-de-Ricard.local>
2023-12-15 20:55:20 +0100

    privatised car  clas

--- js/Car.js
+++ js/Car.js
@@ -2,17 +2,22 @@ import { SPEED_LIMIT, SPEED_UP, SPEED_DOWN, SPEED_INERTIA, TURN_AMOUNT } from '.
 import { Trigonometry } from './utils/trigonometry.js';
 
 class Car {
+
+    #carDom
+    #speed
+    #angle
+
     constructor(carDom) {
-        this.carDom = carDom;
-        this.speed = 0;
-        this.angle = 0;
+        this.#carDom = carDom;
+        this.#speed = 0;
+        this.#angle = 0;
         this.x = 0; 
         this.y = 0; 
     }
 
     reset() {
-        this.speed = 0;
-        this.angle = 0;
+        this.#speed = 0;
+        this.#angle = 0;
         this.x = 0; 
         this.y = 0; 
         this.updatePosition();
@@ -26,26 +31,26 @@ class Car {
 
     updateSpeed(keyList) {
         if (keyList.includes('ArrowUp')) {
-            this.speed = Math.min(this.speed + SPEED_UP, SPEED_LIMIT);
+            this.#speed = Math.min(this.#speed + SPEED_UP, SPEED_LIMIT);
         } else if (keyList.includes('ArrowDown')) {
-            this.speed = Math.max(this.speed - SPEED_DOWN, -SPEED_LIMIT);
+            this.#speed = Math.max(this.#speed - SPEED_DOWN, -SPEED_LIMIT);
         } else {
-            this.speed *= SPEED_INERTIA; 
+            this.#speed *= SPEED_INERTIA; 
         }
     }
 
     updateAngle(keyList) {
         if (keyList.includes('ArrowLeft')) {
-            this.angle -= TURN_AMOUNT;
+            this.#angle -= TURN_AMOUNT;
         } else if (keyList.includes('ArrowRight')) {
-            this.angle += TURN_AMOUNT;
+            this.#angle += TURN_AMOUNT;
         }
     }
 
     move() {
-        const radianAngle = Trigonometry.toRadians(this.angle);
-        const deltaX = this.speed * Math.cos(radianAngle);
-        const deltaY = this.speed * Math.sin(radianAngle);
+        const radianAngle = Trigonometry.toRadians(this.#angle);
+        const deltaX = this.#speed * Math.cos(radianAngle);
+        const deltaY = this.#speed * Math.sin(radianAngle);
 
         this.x += deltaX;
         this.y += deltaY;
@@ -54,7 +59,7 @@ class Car {
     }
 
     updatePosition() {
-        this.carDom.style.transform = `translate(${this.x}px, ${this.y}px) rotate(${this.angle}deg)`;
+        this.#carDom.style.transform = `translate(${this.x}px, ${this.y}px) rotate(${this.#angle}deg)`;
     }
 }
 
